f79d44244ab3bcc86c2f3030feb8d1deb4015b55
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-04-21 14:48:32 +0900

    タブのアタッチ処理を追加

--- Michiakr/app/src/main/java/com/amebaownd/pikohan_niwatori/michiakr/MainActivity.kt
+++ Michiakr/app/src/main/java/com/amebaownd/pikohan_niwatori/michiakr/MainActivity.kt
@@ -2,11 +2,23 @@ package com.amebaownd.pikohan_niwatori.michiakr
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.TabLayout
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setTabLayout()
+    }
+
+    private fun setTabLayout(){
+        val adapter = TabAdapter(supportFragmentManager,this)
+        viewPager.adapter = adapter
+        tabLayout.setupWithViewPager(viewPager)
+        val tab:TabLayout.Tab = tabLayout.getTabAt(0)!!
+        tab.customView = adapter.getTabView(tabLayout,0)
     }
 }
