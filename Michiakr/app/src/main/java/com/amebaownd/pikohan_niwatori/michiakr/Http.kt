f788a2ab9223deca292d0cde70f795e81b5eea32
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-04-21 21:59:43 +0900

    httpコネクト用のmethod追加

---
+++ Michiakr/app/src/main/java/com/amebaownd/pikohan_niwatori/michiakr/Http.kt
@@ -0,0 +1,21 @@
+package com.amebaownd.pikohan_niwatori.michiakr
+
+import java.io.BufferedInputStream
+import java.net.URL
+import javax.net.ssl.HttpsURLConnection
+
+fun httpGet(url:String):BufferedInputStream?{
+    val connect = URL(url).openConnection() as HttpsURLConnection
+    connect.apply {
+        requestMethod="GET"
+        connectTimeout=10000
+        readTimeout = 10000
+        instanceFollowRedirects = true
+    }
+    connect.connect()
+
+    if(connect.responseCode in 200..299){
+        return BufferedInputStream(connect.inputStream)
+    }
+    return null
+}
