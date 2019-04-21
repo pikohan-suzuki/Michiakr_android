e771c6a815009cab060db8010266a13d306dba64
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-04-21 14:46:58 +0900

    テスト用のフラグメントを追加

---
+++ Michiakr/app/src/main/java/com/amebaownd/pikohan_niwatori/michiakr/TestFragment.kt
@@ -0,0 +1,25 @@
+package com.amebaownd.pikohan_niwatori.michiakr
+
+import android.content.Context
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+class TestFragment() : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.test_fragment,container,false)
+        val text = view.findViewById<TextView>(R.id.fragment_text)
+        text.text = "Success"
+        text.textAlignment= View.TEXT_ALIGNMENT_CENTER
+        return view
+    }
+}
