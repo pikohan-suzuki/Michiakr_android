a3159f356b08c8aa2066013497da916a772c0237
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-04-21 14:48:04 +0900

    記事ソート用のタブを追加

---
+++ Michiakr/app/src/main/java/com/amebaownd/pikohan_niwatori/michiakr/TabAdapter.kt
@@ -0,0 +1,34 @@
+package com.amebaownd.pikohan_niwatori.michiakr
+
+import android.content.Context
+import android.support.design.widget.TabLayout
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentStatePagerAdapter
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.TextView
+
+class TabAdapter(fm: FragmentManager, private val context: Context) : FragmentStatePagerAdapter(fm) {
+
+    private val inflater = LayoutInflater.from(context)
+    private val title = arrayOf<String>("最新の記事", "人気の記事")
+    override fun getItem(position: Int): Fragment? {
+        return TestFragment()
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return title[position]
+    }
+
+    override fun getCount() = title.size
+
+    fun getTabView(tabLayout: TabLayout, position: Int) : View {
+        val view = inflater.inflate(R.layout.tab_item, tabLayout, false)
+        val tab = view.findViewById<TextView>(R.id.tab_name)
+        tab.text = getPageTitle(position)
+        tab.textAlignment=View.TEXT_ALIGNMENT_CENTER
+        
+        return view
+    }
+}
