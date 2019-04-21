dd7d9fc6631af14a11f8998b50032bada6aebd20
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-04-21 21:59:11 +0900

    Contents表示用のAdapterを追加

---
+++ Michiakr/app/src/main/java/com/amebaownd/pikohan_niwatori/michiakr/ContentsAdapter.kt
@@ -0,0 +1,24 @@
+package com.amebaownd.pikohan_niwatori.michiakr
+
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+
+class ContentsAdapter(private val context: Context, private val contents:ArrayList<Contents>):BaseAdapter() {
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+
+    }
+
+    override fun getItem(position: Int): Any {
+
+    }
+
+    override fun getItemId(position: Int): Long {
+
+    }
+
+    override fun getCount(): Int {
+
+    }
+}
